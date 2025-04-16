7113517d2bb4e361e1f8aa793d7b1af1c224e28b
Harry Lai <145167875+harry-lai@users.noreply.github.com>
2025-04-16 01:11:47 -0700

    Update JournalEntry.js

--- JournalEntry.js
+++ JournalEntry.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const journalEntrySchema = new mongoose.Schema({
-  title: { type: String, required: true }, // Title field
+  title: { type: String, required: true }, 
   text: { type: String, required: true },
   sentiment: { type: Number, required: true },
   createdAt: { type: Date, default: Date.now },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // Associate with user
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, 
 });
 
 const JournalEntry = mongoose.model('JournalEntry', journalEntrySchema);
